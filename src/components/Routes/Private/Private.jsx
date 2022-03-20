368d93d2bb1b4183d6d41b9a82d10d24c8651214
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-03-20 18:01:35 -0300

    Implementação de Rotas privadas = login

---
+++ src/components/Routes/Private/Private.jsx
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import StoreContext from 'components/store/Context';
+
+const RoutesPrivate = ({component: Component, ...rest}) => {
+    const { token } = useContext(StoreContext);
+
+    return(
+        <Route 
+            {...rest}
+            render={() => token
+                ? <Component {...rest}/>
+                : <Redirect to={/login}/>
+            }
+        />
+    )
+
+}
